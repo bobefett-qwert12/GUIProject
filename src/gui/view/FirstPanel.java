0dcfab6952439e70f8351c74c8ce8ee526655207
RyanPerkins <bobefett.qwert12@gmail.com>
2018-11-20 09:49:58 -0700

    added a layout and a listener for the button  to change color

--- src/gui/view/FirstPanel.java
+++ src/gui/view/FirstPanel.java
@@ -2,14 +2,25 @@ package gui.view;
 
 import javax.swing.*;
 import gui.controller.GUIController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FirstPanel extends JPanel
 {
 	private GUIController appController;
+	private JButton myButton;
+	private JLabel myLabel;
+	private SpringLayout appLayout;
+	
 	public FirstPanel(GUIController appController)
 	{
 		super();
 		this.appController = appController;
+		
+		myButton = new JButton("Click Me!");
+		myLabel = new JLabel("Text on a screen.");
+		appLayout = new SpringLayout();
 		
 		setupPanel();
 		setupLayout();
@@ -18,7 +29,9 @@ public class FirstPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.add(myButton);
+		this.add(myLabel);
+		this.setLayout(appLayout);
 	}
 	
 	private void setupLayout()
@@ -26,8 +39,23 @@ public class FirstPanel extends JPanel
 		
 	}
 	
+	private void changeColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
+	}
+	
 	private void setupListeners()
 	{
-		
+		myButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				changeColor();
+			}
+		});
 	}
 }
